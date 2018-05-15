cd2a0be822a8b27e7a1e5fa783589df416d80162
Daniel Ramos <ramos.danielferreira@outlook.com>
2018-05-14 21:00:00 -0300

    Respostas dos exercicios 01 a 07

---
+++ Respostas/Aula01Aula02/Exercicio03/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Exercicio03
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var numerosParaCalculo = new[] {1, 2, 3, 4, 5, 95, 96, 97, 98, 99, 100};
+            int somatorio = default(int);
+            foreach (int numero in numerosParaCalculo)
+                if (numero > 95)
+                    somatorio += numero;
+            Console.WriteLine(somatorio);
+        }
+    }
+}
---
+++ Respostas/Aula01Aula02/Exercicio05/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exercicio05
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var indice = 1;
+            int somatorio = default(int);
+            while (indice <= 100)
+            {
+                somatorio += indice;
+                indice++;
+            }
+
+            Console.WriteLine(somatorio);
+        }
+    }
+}
